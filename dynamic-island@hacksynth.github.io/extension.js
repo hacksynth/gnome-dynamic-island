78557decd5f69a2a6b7e690a88e7e1dec1f458fc
hacksynth <hacksynth@outlook.com>
2026-04-19 20:07:06 +0800

    chore: scaffold extension skeleton and node test runner

---
+++ dynamic-island@hacksynth.github.io/extension.js
@@ -0,0 +1,11 @@
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+
+export default class DynamicIslandExtension extends Extension {
+    enable() {
+        // Wired in Task 9.
+    }
+
+    disable() {
+        // Wired in Task 9.
+    }
+}
