5cfa3b03e5e73908b675f64ccd988df8660f2d0c
hacksynth <hacksynth@outlook.com>
2026-04-19 20:26:20 +0800

    feat(view): panel integration with DateMenu hide/restore

---
+++ dynamic-island@hacksynth.github.io/src/panel-integration.js
@@ -0,0 +1,42 @@
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+export class PanelIntegration {
+    constructor(view) {
+        this._view = view;
+        this._dateMenu = Main.panel.statusArea.dateMenu;
+        this._dateMenuParent = null;
+        this._mounted = false;
+    }
+
+    mount() {
+        if (this._mounted) return;
+        const center = Main.panel._centerBox;
+
+        if (this._dateMenu) {
+            this._dateMenuParent = this._dateMenu.container.get_parent();
+            if (this._dateMenuParent) this._dateMenu.container.hide();
+        }
+
+        center.add_child(this._view);
+        this._mounted = true;
+
+        // Warn (don't fight) if other extensions added children.
+        const siblings = center.get_children().filter(c => c !== this._view
+            && (!this._dateMenu || c !== this._dateMenu.container));
+        if (siblings.length > 0) {
+            Main.notify('Dynamic Island',
+                `Detected ${siblings.length} other center-box extension(s). Layout may be cramped.`);
+        }
+    }
+
+    unmount() {
+        if (!this._mounted) return;
+        const center = Main.panel._centerBox;
+        if (this._view.get_parent() === center) center.remove_child(this._view);
+
+        if (this._dateMenu && this._dateMenuParent) this._dateMenu.container.show();
+        this._mounted = false;
+    }
+
+    destroy() { this.unmount(); }
+}
